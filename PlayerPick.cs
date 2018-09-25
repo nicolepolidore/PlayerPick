dca708104923672b149aff426ba1f7346b7b473c
Nicole Polidore <42782119+nicolepolidore@users.noreply.github.com>
2018-09-25 00:50:55 -0400

    Add files via upload

---
+++ PlayerPick.cs
@@ -0,0 +1,45 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerPick: MonoBehaviour
+{
+
+   
+    public Text countText;
+    public Text gameOverText;
+  
+
+    
+    private int count;
+
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+       
+    }
+
+   
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            gameOverText.text = "GAME OVER!";
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+       
+    }
+}
